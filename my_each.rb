a5456086008431a0b597cb9c3809017a0d4a3d1b
Isaiah Montoya <idmontoya@yahoo.com>
2021-03-04 19:11:20 -0900

    commit tree fa420027ede7beec211d407d29d012d551f84c20

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+names = ["Zach", "Don", "Erik"]
+
+def my_each(names) # put argument(s) here
+  i = 0 
+  
+  while i < names.length
+    yield(names[i])
+    i = i + 1
+  end
+  
+  
+  names # code here
+  
 end
